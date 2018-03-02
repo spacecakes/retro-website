c1b74475f7e885be71ad4e05c85d3df3843be545
Gabriel <g.lundmark@gmail.com>
2018-03-02 11:36:27 +0100

    pause "midi" on WAP site

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,26 @@
-// I'm sorry for this. So sorry.
-document.onload = () => {
-  document.querySelector("audio").play();
-};
-
 // Change to WAP site at 200px width
-let image = document.getElementById("me");
-let headline = document.getElementById("headline");
-let windowWidth = window.matchMedia("(max-width: 320px)");
+const image = document.getElementById("me");
+const headline = document.getElementById("headline");
+const windowWidth = window.matchMedia("(max-width: 320px)");
+const midi = document.querySelector("audio");
+
 function mediaQuery(windowWidth) {
   if (windowWidth.matches) {
     image.src = "assets/me-wap.png";
     headline.innerText = "Welcome to my WAP page";
+    midi.pause();
   } else {
     image.src = "assets/me.gif";
     headline.innerText = "Welcome to my homepage";
+    midi.play();
   }
 }
 
 mediaQuery(windowWidth);
 windowWidth.addListener(mediaQuery);
+
+/* // I'm sorry for this. So sorry.
+document.onload = () => {
+  midi.play();
+};
+ */
