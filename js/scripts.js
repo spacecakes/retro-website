ca51ab57eb09bcb9786d5c32d0c128c99fab74ee
Gabriel <g.lundmark@gmail.com>
2018-03-28 18:12:22 +0200

    very basic clippy integration

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 // Change to WAP site at 200px width
-const image = document.getElementById("me");
-const headline = document.getElementById("headline");
-const windowWidth = window.matchMedia("(max-width: 320px)");
-const midi = document.querySelector("audio");
+const image = document.getElementById('me');
+const headline = document.getElementById('headline');
+const windowWidth = window.matchMedia('(max-width: 320px)');
+const midi = document.querySelector('audio');
 
 function wap(windowWidth) {
   if (windowWidth.matches) {
-    image.src = "assets/me-wap.png";
-    headline.innerText = "Welcome to my WAP page";
+    image.src = 'assets/me-wap.png';
+    headline.innerText = 'Welcome to my WAP page';
     midi.pause();
   } else {
-    image.src = "assets/me.gif";
-    headline.innerText = "Welcome to my homepage";
+    image.src = 'assets/me.gif';
+    headline.innerText = 'Welcome to my homepage';
     midi.play();
   }
 }
@@ -23,3 +23,10 @@ windowWidth.addListener(wap);
 document.onload = () => {
   midi.play();
 };
+
+// Clippy
+clippy.load('Clippy', function (agent) {
+  // do anything with the loaded agent
+  agent.show();
+  agent.animate();
+});
